e85ce22bb9bd71d08d3b57b74063ec434d580542
kenji-special <k.saito.1985@gmail.com>
2013-11-18 08:40:55 -0500

    added timer in home view.

--- app/scripts/views/timerView.js
+++ app/scripts/views/timerView.js
@@ -13,7 +13,8 @@ define([
     var TimerView = Backbone.View.extend({
         el                      : "#main-container",
         $timer                  : null,
-        timerStartHandlerAction : null,
+        timerLoopHandlerAction  : null,
+        timerLoopStatus         : false,
 
         today                   : null,
         objectId                : null,
@@ -25,27 +26,56 @@ define([
             console.log("timer view initialize");
             //this.timerStartHandlerAction = _.bind( this.timerStartHandler, this );
             //myEvent.on(myEvent.TIMER_START, this.timerStartHandlerAction);
+            this.timerLoopHandlerAction = _.bind( this.timerLoopHandler, this);
         },
 
         timerStartAction : function( objectId, name){
             //projectCollection.getObject(_objectId);
-            this.timerValue = 0;
+            this.timerValue      = 0;
+            this.timerLoopStatus = true;
 
             this.objectId = objectId;
             this.name     = name;
 
-            this.today = moment().format("MMM D YYYY");
+            this.today = new Date().getTime();
 
             var $projectName = this.$el.find("#timer .title");
             $projectName.html(name);
 
             var $date = this.$el.find("#timer .date")
-            $date.html(this.today);
+            $date.html(moment().format("MMM D YYYY"));
 
 
             this.$timer = this.$el.find("#timer");
             this.$timer.removeClass("inactive").addClass("active");
+
+            /// timer start
+            this.$hourAndMinutes = this.$el.find("#timer .hour");
+            this.$seconds        = this.$el.find("#timer .second");
+            this.timerLoopHandler();
+        },
+
+        timerLoopHandler : function(){
+            //this.timerValue += 1;
+            this.timerValue += 1000;
+            var duration = moment.duration( this.timerValue, 'milliseconds');
+
+            var seconds = duration.seconds(),
+                minutes = duration.minutes(),
+                hours   = duration.hours();
+
+            var secondString = seconds < 10 ? "0" + seconds : seconds;
+            var minuteString = minutes < 10 ? "0" + minutes : minutes;
+            var hourString   = hours   < 10 ? "0" + hours : hours;
+
+            // moment
+            this.$hourAndMinutes.html(hourString + " : " + minuteString);
+            this.$seconds.html(secondString);
+
+            if(this.timerLoopStatus) setTimeout(this.timerLoopHandlerAction, 1000);
         }
+
+
 
     });
 
